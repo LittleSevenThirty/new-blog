5b80f65b6c53fd34169d367848186353e7b63840
Qilin Zhong <QLZZ@nnit.com>
2025-11-14 17:14:57 +0800

    回家了

---
+++ blog_frontend/littleseventhirty-blog/src/components/Loading/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="" v-show="loadingStore.isLoading">
+        <!-- 整个屏幕大的盒子 -->
+        <div>
+            <!-- 中间一个小盒子 -->
+            <img>我需要一张图片</img>
+            <span>
+                <!-- 最后一个盒子 -->
+                正在带着数据走向你~~
+            </span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import useLoadingStore from '../../pinia/store/modules/loading';
+const loadingStore=useLoadingStore();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- blog_frontend/littleseventhirty-blog/vite.config.ts
+++ blog_frontend/littleseventhirty-blog/vite.config.ts
@@ -11,7 +11,14 @@ export default defineConfig({
     Components({
       // 重要配置：指定类型声明文件输出路径
       dts: "src/types/components.d.ts", 
-      
+      // 其它配置1：指定需要扫描的组件目录（插件会扫描这些文件下的.vue文件）默认src/components
+      dirs: [
+        "src/components",
+      ],
+      // 其它配置2：解析器配置=》对于其它第三方组件库，需要对应的解析器
+      resolvers: [
+
+      ]
     }),// 添加unplugin-vue-components配置
   ],
   resolve: {
