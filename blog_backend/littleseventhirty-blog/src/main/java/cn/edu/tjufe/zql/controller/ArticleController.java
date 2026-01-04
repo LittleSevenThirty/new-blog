34136741f6fa477f50fd5a439718a0a377de36dd
Qilin Zhong <QLZZ@nnit.com>
2026-01-04 09:20:58 +0800

    2025/12/31日未提交

--- blog_backend/littleseventhirty-blog/src/main/java/cn/edu/tjufe/zql/controller/ArticleController.java
+++ blog_backend/littleseventhirty-blog/src/main/java/cn/edu/tjufe/zql/controller/ArticleController.java
@@ -6,12 +6,18 @@ import cn.edu.tjufe.zql.domain.entity.Category;
 import cn.edu.tjufe.zql.domain.response.ResponseResult;
 import cn.edu.tjufe.zql.domain.vo.SearchArticleByContentVO;
 import cn.edu.tjufe.zql.service.IArticleService;
+import cn.edu.tjufe.zql.utils.ResponseWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,8 +32,21 @@ public class ArticleController {
     @Resource(name="articleService")
     private IArticleService articleService;
 
+    /**
+     * 根据内容片段搜索文章
+     * @param content 搜索文章内容片段
+     * @return
+     */
+    @Operation(summary="根据内容片段搜索文章")
+    @Parameters({
+            @Parameter(name="content",description = "搜索文章内容片段",required = true)
+    })
     @GetMapping("search/by/content")
-    public void searchArticleByContent(String content){
-//        ResponseResult<List<SearchArticleByContentVO>>(articleService.searchArticleByContent(content));
+    public ResponseResult searchArticleByContent(
+            @NotEmpty
+            @Length()
+            @RequestParam("content")
+            String content){
+        return ResponseWrapper.handler(()->articleService.searchArticleByContent(content));
     }
 }
