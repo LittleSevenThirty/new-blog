e9ff068348cc4fc907da4446b6553bc754a2b41f
Zhongcream <3100649706@qq.com>
2026-01-08 22:53:19 +0800

    回家一些修改

---
+++ blog_backend/littleseventhirty-blog/src/main/java/cn/edu/tjufe/zql/controller/WebsiteInfoController.java
@@ -0,0 +1,15 @@
+package cn.edu.tjufe.zql.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @authro 钟奇林
+ * @description
+ * @date 2026/1/8
+ * @github https://github.com/little-seven-thirty
+ */
+@Controller(value="websiteInfoController")
+public class WebsiteInfoController {
+}
--- blog_backend/littleseventhirty-blog/src/main/java/cn/edu/tjufe/zql/service/impl/WebsiteInfoService.java
+++ blog_backend/littleseventhirty-blog/src/main/java/cn/edu/tjufe/zql/service/impl/WebsiteInfoService.java
@@ -1,11 +1,18 @@
 package cn.edu.tjufe.zql.service.impl;
 
+import cn.edu.tjufe.zql.domain.entity.WebsiteInfo;
+import cn.edu.tjufe.zql.mapper.WebsiteInfoMapper;
 import cn.edu.tjufe.zql.service.IWebsiteInfoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: littleseventhirty
  * @description:
  * @date: 2026/1/8-17:25
  **/
-public class WebsiteInfoService implements IWebsiteInfoService extends  {
+@Slf4j
+@Service
+public class WebsiteInfoService extends ServiceImpl<WebsiteInfoMapper, WebsiteInfo> implements IWebsiteInfoService  {
 }
